ebadf3f0b63b7ac092f9662b6b760b3919e9a314
Sebastian Hempel Linde <sebastian@hempellinde.com>
2025-05-30 22:14:04 +0200

    update

--- init.el
+++ init.el
@@ -1,8 +1,70 @@
 ;;; init.el --- SHLinde Emacs Configuration -*- lexical-binding: t; -*-
+;;; Code:
 
-
+;;; Core
+(defconst shl--cache-dir (concat user-emacs-directory ".cache/"))
 
 ;;; User Experience
+;;;; UI
+;;;;; Theme
+(use-package modus-themes
+  :ensure t
+  :hook (after-init . (lambda () (load-theme 'modus-vivendi :no-confirm))))
+
+;;;;; Fonts
+(set-face-attribute 'default nil
+		    :family "Aporetic Sans Mono"
+		    :height 110
+		    :weight 'medium)
+
+;;;;; Modeline
+(use-package time
+  :ensure nil
+  :hook (after-init . display-time-mode))
+
+(use-package battery
+  :ensure nil
+  :hook (after-init . display-battery-mode))
+
+;;;; Editor
+;; Ensure keyboard repeat rate
+(shell-command "xset r rate 250 60")
+
+;; Send custom file to oblivion
+(setq custom-file (make-temp-file "emacs-custom-"))
+
+;; Don't generate backups or lockfiles. While auto-save maintains a copy so long
+;; as a buffer is unsaved, backups create copies once, when the file is first
+;; written, and never again until it is killed and reopened. This is better
+;; suited to version control, and I don't want world-readable copies of
+;; potentially sensitive material floating around our filesystem.
+(setq create-lockfiles nil
+      make-backup-files nil
+      ;; But in case the user does enable it, some sensible defaults:
+      version-control t     ; number each backup file
+      backup-by-copying t   ; instead of renaming current file (clobbers links)
+      delete-old-versions t ; clean up after itself
+      kept-old-versions 5
+      kept-new-versions 5
+      backup-directory-alist (list (cons "." (concat shl--cache-dir "backup/")))
+      tramp-backup-directory-alist backup-directory-alist)
+
+;; But turn on auto-save, so we have a fallback in case of crashes or lost data.
+;; Use `recover-file' or `recover-session' to recover them.
+(setq auto-save-default t
+      ;; Don't auto-disable auto-save after deleting big chunks. This defeats
+      ;; the purpose of a failsafe. This adds the risk of losing the data we
+      ;; just deleted, but I believe that's VCS's jurisdiction, not ours.
+      auto-save-include-big-deletions t
+      ;; Keep it out of `user-emacs-dir' or the local directory.
+      auto-save-list-file-prefix (concat shl--cache-dir "autosave/")
+      tramp-auto-save-directory  (concat shl--cache-dir "tramp-autosave/")
+      auto-save-file-name-transforms
+      (list (list "\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'"
+                  ;; Prefix tramp autosaves to prevent conflicts with local ones
+                  (concat auto-save-list-file-prefix "tramp-\\2") t)
+            (list ".*" auto-save-list-file-prefix t)))
+
 ;; Smooth scrolling
 (use-package ultra-scroll
   :vc (:url "https://github.com/jdtsmith/ultra-scroll") ; For Emacs>=30
@@ -12,23 +74,115 @@
   :config
   (ultra-scroll-mode 1))
 
+
+;;; Built-in Plugins
+(use-package autorevert
+  ;; revert buffers when their files/state have changed
+  :config
+  (auto-revert-mode)
+  (setq auto-revert-verbose t ; let us know when it happens
+        auto-revert-use-notify nil
+        auto-revert-stop-on-user-input nil
+        ;; Only prompts for confirmation when buffer is unsaved.
+        revert-without-query (list ".")))
+
+(use-package savehist
+  ;; persist variables across sessions
+  :hook (emacs-startup . savehist-mode)
+  ;; :custom (savehist-file (concat shl--cache-dir "savehist"))
+  :config
+  (setq savehist-save-minibuffer-history t
+        savehist-autosave-interval nil     ; save on kill only
+        savehist-additional-variables
+        '(kill-ring                        ; persist clipboard
+          register-alist                   ; persist macros
+          mark-ring global-mark-ring       ; persist marks
+          search-ring regexp-search-ring)) ; persist searches
+  (defun shl--savehist-remove-unprintable-registers-h ()
+    "Remove unwriteable registers (e.g. containing window configurations).
+Otherwise, `savehist' would discard `register-alist' entirely if we don't omit
+the unwritable tidbits."
+    ;; Save new value in the temp buffer savehist is running
+    ;; `savehist-save-hook' in. We don't want to actually remove the
+    ;; unserializable registers in the current session!
+    (setq-local register-alist
+                (cl-remove-if-not #'savehist-printable register-alist)))
+  (defun shl--savehist-unpropertize-variables-h ()
+    "Remove text properties from `kill-ring' to reduce savehist cache size."
+    (setq kill-ring
+          (mapcar #'substring-no-properties
+                  (cl-remove-if-not #'stringp kill-ring))
+          register-alist
+          (cl-loop for (reg . item) in register-alist
+                   if (stringp item)
+                   collect (cons reg (substring-no-properties item))
+                   else collect (cons reg item))))
+  (add-hook 'savehist-save-hook #'shl--savehist-remove-unprintable-registers-h)
+  (add-hook 'savehist-save-hook #'shl--savehist-unpropertize-variables-h))
+
+(use-package delsel
+  :ensure nil
+  :hook (after-init . delete-selection-mode))
+
+(use-package subword
+  :ensure nil
+  :hook (after-init . global-subword-mode))
+
+(use-package window
+  :ensure nil
+  :bind (("M-o" . other-window)))
+
+;; Expand Region
+(use-package expand-region
+  :ensure t
+  :bind ("M-h" . er/expand-region))
+
+;; Multiple Cursors Emacs
+(use-package multiple-cursors
+  :ensure t)
+
+;; Which-key
+(use-package which-key
+  :defer 1
+  :ensure t
+  :init
+  (setq which-key-idle-delay 0.3)
+  :config
+  (setq which-key-sort-order 'which-key-prefix-then-key-order
+        which-key-sort-uppercase-first nil
+        which-key-max-display-columns 5)
+  (which-key-mode))
+
+;;;; Minibuffer Completion
+(use-package vertico
+  :ensure t
+  :hook (after-init . vertico-mode))
+
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
+;;; Compilation
+;; Colorful Compilation
+(add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (ansi-color-apply-on-region compilation-filter-start (point)))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+
+;;;; Environment Variables
+(use-package exec-path-from-shell
+  :ensure t
+  :hook (emacs-startup . exec-path-from-shell-initialize)
+  :config (exec-path-from-shell-copy-env "ANTROPIC_API_KEY"))
+
 ;;; Version Control
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)))
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit ultra-scroll))
- '(package-vc-selected-packages
-   '((ultra-scroll :url "https://github.com/jdtsmith/ultra-scroll"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(provide 'init)
+;;; init.el ends here
