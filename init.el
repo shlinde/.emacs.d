75225995cf72c1e6c63f32957e9066612601a137
Sebastian Hempel Linde <stl@yco.dk>
2025-07-09 19:45:09 +0200

    update

--- init.el
+++ init.el
@@ -351,6 +351,48 @@ the unwritable tidbits."
   ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
 )
 
+(use-package embark
+  :ensure t
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+
+
+(use-package activities
+  :ensure t
+  :init
+  (activities-mode)
+  (activities-tabs-mode)
+  ;; Prevent `edebug' default bindings from interfering.
+  (setq edebug-inhibit-emacs-lisp-mode-bindings t)
+  :bind
+  (("C-x C-a C-n" . activities-new)
+   ("C-x C-a C-d" . activities-define)
+   ("C-x C-a C-a" . activities-resume)
+   ("C-x C-a C-s" . activities-suspend)
+   ("C-x C-a C-k" . activities-kill)
+   ("C-x C-a RET" . activities-switch)
+   ("C-x C-a b" . activities-switch-buffer)
+   ("C-x C-a g" . activities-revert)
+   ("C-x C-a l" . activities-list)))
+
 ;;; Compilation
 ;; Colorful Compilation
 (add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
@@ -449,13 +491,9 @@ Looks for .venv directory in project root and activates the Python interpreter."
   :ensure t
   :config
   (general-evil-setup t)
-
   (general-create-definer shl/leader-keys
-
     :keymaps '(normal insert visual emacs)
-
     :prefix "SPC"
-
     :global-prefix "C-SPC")
 
   (shl/leader-keys
@@ -467,6 +505,16 @@ Looks for .venv directory in project root and activates the Python interpreter."
    "pg" #'project-find-regexp
    "g" '(:ignore t :which-key "git")
    "gg" #'magit-status
+   "a" '(:ignore t :which-key "activities")
+   "an" #'activities-new
+   "ad" #'activities-define
+   "aa" #'activities-resume
+   "as" #'activities-suspend
+   "ak" #'activities-kill
+   "a RET" #'activities-switch
+   "ab" #'activities-switch-buffer
+   "ag" #'activities-revert
+   "al" #'activities-list
    ))
 
 
@@ -508,6 +556,7 @@ Looks for .venv directory in project root and activates the Python interpreter."
   :ensure t
   :after evil
   :config (evil-collection-init))
+
 
 
 
